22ec5478798f00af3629208153b29fd7ac557337
Ivan Assumpcao <Ivan.Assumpcao@landisgyr.com>
2024-01-29 12:39:37 -0300

    Testes unitários

---
+++ TributoJusto/tests/TributoJusto.Tests/UnitTests/Filmes/FilmeTests.cs
@@ -0,0 +1,47 @@
+﻿using TributoJusto.Business.Validations;
+using TributoJusto.Tests.UnitTests.Fixtures;
+
+namespace TributoJusto.Tests.UnitTests.Filmes
+{
+    [Collection(nameof(FilmeCollection))]
+    public class FilmeTests
+    {
+        private readonly FilmeTestsFixture _FilmeTestsFixture;
+
+        public FilmeTests(FilmeTestsFixture FilmeTestsFixture)
+        {
+            _FilmeTestsFixture = FilmeTestsFixture;
+        }
+
+        [Fact(DisplayName = "Novo Filme Válido")]
+        [Trait("Categoria", "Filme Fixture Tests")]
+        public void Filme_NewFilme_MustBeValid()
+        {
+            // Arrange
+            var filme = _FilmeTestsFixture.CriarFilmeValido();
+
+            // Act
+            var result = new FilmeValidation().Validate(filme);
+
+            // Assert
+            Assert.True(result.IsValid);
+            Assert.Empty(result.Errors);
+        }
+
+        [Fact(DisplayName = "Novo Filme Inválido")]
+        [Trait("Categoria", "Filme Fixture Tests")]
+        public void Filme_NewFilme_MustBeInvalid()
+        {
+            // Arrange
+            var filme = _FilmeTestsFixture.CriarFilmeInvalido();
+
+            // Act
+            var result = new FilmeValidation().Validate(filme);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.NotEmpty(result.Errors);
+            Assert.Equal(2, result.Errors.Count);
+        }
+    }
+}
